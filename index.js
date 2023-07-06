a2ea167e0b4a1a201664cbc6cf109ab53940bbb8
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-07-06 14:58:29 -0300

    comence a agregar las rutas

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import cors from 'cors'
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
 
-//configurar un puerto
+dotenv.config();//puedo leer variables de entorno
 //crear una instancia de express
-
 const app = express();
 
+//configurar un puerto
 app.set('port', process.env.PORT || 4000);
 app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puereto' + app.get('port'));
+});
+
+//middelewares: funciones que ejecutan alguna tarea antes de llegar a las rutas
+app.use(cors()); //permite conexiones remotas
+app.use(express.json()); //permite interpretar el formato json
+app.use(express.urlencoded({extended: true})); //permite en el objeto request los strings y arrays
+app.use(morgan('dev')); //nos da una info extra en la terminal
+
+
+//rutas
+app.get('/prueba', (req, res)=>{
+    res.send('esto es una prueba de la peticion GET a mi backend');
 });
